92cfc40232230e6317ea66cf7575db721a64859c
AnnieVV1904 <Andrea.Villarroel@fundacion-jala.org>
2022-12-19 13:05:24 -0400

    class 4 : Power_ups

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -3,6 +3,7 @@ import pygame
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 
 class Game:
@@ -20,11 +21,15 @@ class Game:
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
         self.points = 0
+        self.power_up_manager = PowerUpManager()
+
 
     def score(self):
         self.points += 1
         if self.points % 100 == 0:
             self.game_speed += 1
+
+        print(self.points)
 
     def run(self):
         # Game loop: events - update - draw
@@ -44,7 +49,10 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self.game_speed, self)
+        self.power_up_manager.update(self.points, self.game_speed, self.player) 
         self.score()
+        self.player.check_invincibility()
+        
 
     def draw(self):
         self.clock.tick(FPS)
@@ -52,6 +60,7 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
